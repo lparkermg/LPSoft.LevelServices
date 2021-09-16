cb2a4b8c4caff332d66b2ca89cc9dd6c61b53c2e
Luke Parker <moongateuk@gmail.com>
2021-09-16 23:12:28 +0100

    Adds initial project setup + codestyles.

---
+++ src/LevelServices/Interfaces/ILevelService.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="ILevelService.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace LevelServices.Interfaces
+{
+    /// <summary>
+    /// Interface for a level service implementation.
+    /// </summary>
+    /// <typeparam name="TMapData">The data type for map files.</typeparam>
+    public interface ILevelService<TMapData>
+    {
+    }
+}
---
+++ src/LevelServices/LevelService.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="Class1.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace LevelServices
+{
+    using LevelServices.Interfaces;
+
+    public class LevelService: ILevelService<string>
+    {
+    }
+}
