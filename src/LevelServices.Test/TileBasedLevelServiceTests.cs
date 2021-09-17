f2a8bb50b8b7b068f8ac78f274309cf13d6ad070
Luke Parker <lparkermg@outlook.com>
2021-09-17 17:39:31 +0100

    0.0.1: Initial Version (#1)
    * Adds initial project setup + codestyles.
    
    * Starts adding TileBasedLevelService.
    
    * Implements setting the current map.
    
    * Removed un-needed comments.
    
    * Cleaned up code/settings, TileBasedLevelService -> LevelService.
    
    * Updates readme.md
    
    * Adds License to repo.
    
    * Adds build and publish scripts, adds package details.
    
    * Adds explicit -c Release flag to build.
    
    * Adds package description.

---
+++ src/LevelServices.Test/TileBasedLevelServiceTests.cs
@@ -0,0 +1,75 @@
+using NUnit.Framework;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace LevelServices.Test
+{
+    public class TileBasedLevelServiceTests
+    {
+        [Test]
+        public void SetBaseMaps_GivenNullMapData_ThrowsArgumentNullException()
+        {
+            var service = new LevelService<string>();
+            Assert.That(() => service.SetBaseMaps(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void SetBaseMaps_GivenValidMapData_SetsBaseMaps()
+        {
+            var expectedMaps = new[] { "String 1", "Test 2", "Array Item 3" };
+
+            var service = SetupService(expectedMaps);
+
+            Assert.That(service.BaseMaps, Is.EqualTo(expectedMaps));
+        }
+
+        [Test]
+        public void SetCurrentMap_GivenNoLoadedMaps_ThrowsArgumentException()
+        {
+            var service = new LevelService<string>();
+            Assert.That(() => service.SetCurrentMap(0), Throws.ArgumentException.With.Message.EqualTo("Cannot set current map, base maps have not been loaded"));
+        }
+
+        [Test]
+        public void SetCurrentMap_GivenNegativeIndex_ThrowsArgumentException()
+        {
+            var service = new LevelService<string>();
+            Assert.That(() => service.SetCurrentMap(-5), Throws.ArgumentException.With.Message.EqualTo("Cannot set current map, index cannot be negative"));
+        }
+
+        [Test]
+        public void SetCurrentMap_GivenIndexMoreThanMaps_ThrowsArgumentException()
+        {
+            var maps = new[] { "String 1", "Test 2", "Array Item 3" };
+
+            var service = SetupService(maps);
+
+            Assert.That(() => service.SetCurrentMap(maps.Length + 5), Throws.ArgumentException.With.Message.EqualTo("Cannot set current map, index is higher than loaded maps length"));
+        }
+
+        [Test]
+        public void SetCurrentMap_GivenValidIndex_SetsCurrentMapProperty()
+        {
+            var maps = new[] { "String 1", "Test 2", "Array Item 3" };
+
+            var service = SetupService(maps);
+            service.SetCurrentMap(1);
+            Assert.That(service.CurrentMap, Is.EqualTo(maps[1]));
+        }
+
+        private LevelService<T> SetupService<T>(T[] dataToSet)
+        {
+            var memStream = new MemoryStream();
+            var serializer = new XmlSerializer(typeof(T[]));
+            serializer.Serialize(memStream, dataToSet);
+
+            // Set to position zero before passing in the stream.
+            memStream.Position = 0;
+            var service = new LevelService<T>();
+
+            service.SetBaseMaps(memStream);
+
+            return service;
+        }
+    }
+}
